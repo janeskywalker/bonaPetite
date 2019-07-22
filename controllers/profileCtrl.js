aa203b56988437c58c5c85c8cb9ef2ff55467bd4
Lawrence <lawrencema415@gmail.com>
2019-07-22 14:07:45 -0700

    adding profileCtrl

---
+++ controllers/profileCtrl.js
@@ -0,0 +1,18 @@
+const db = require('./models');
+
+const showProfile = (req,res) => {
+  if(!req.session.currentUser) {
+    return res.redirect('/accounts/login');
+  }
+
+  db.User.findById(req,session,currentUser._id (error, foundUser) => {
+    if (error) return res.render('index',{errors: [{message:'Something went wrong, please try again'}]});
+
+    res.render('profile/show', {currentUser: foundUser});
+  });
+
+}
+
+module.exports = {
+  showProfile
+}
