e062414f5bf316a62b1c2bf9bf5407f1fb7814b6
Jane Skywalker <janehuizeng@gmail.com>
2019-07-23 09:03:11 -0700

    organizing server.js

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 // Modules
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
-const db = require('./models')
-console.log(db)
-
-// Instance Modules
-const app = express();
-const routes = require('./routes');
-app.set('view engine','ejs');
-
-// Global Variables
 const PORT = 3000;
 
-//Middleware
-app.use(express.json());
+// routes
+const routes = require('./routes');
 
+// Set View Engine
+app.set('view engine', 'ejs');
+
+
+// ------------------------------- MIDDLEWARE  -------------------------------- //
+
+// Serve Public Directory
 app.use(express.static(`${__dirname}/public`));
-
 
 // BodyParser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 
 // use the express Session
 app.use(session({
@@ -32,17 +28,40 @@ app.use(session({
   saveUninitialized: false // Only save the session if a property has been added to req.session
 }));
 
+// Method Override
+// app.use(methodOverride('_method'));
 
+// Custom Logger Middleware
+// app.use((req, res, next) => {
+//   const url = req.url;
+//   const method = req.method;
+//   const requestedAt = new Date().toLocaleString();
+//   console.table({ url, method, requestedAt });
+//   next();
+// });
+
+
+// ------------------------------- HTML ENDPOINTS -------------------------------- //
 //main page
 app.get('/',(req,res)=> {
   res.render('index');
 });
 
-
-
+// acountes routes
 app.use('/accounts',routes.signUp);
+
+// profile routes
 // app.use('/profile',routes.profile);
 
+
+
+// ------------------------------- API ENDPOINTS -------------------------------- //
+
+
+
+
+
+// ------------------------------- START SERVER  -------------------------------- //
 app.listen(PORT, ()=> {
   console.log("Server is working");
 })
