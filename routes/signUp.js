7674696b6653256fc34858c37a814a5d9e9cbf06
Jane Skywalker <janehuizeng@gmail.com>
2019-07-22 17:02:05 -0700

    hooking up login page

--- routes/signUp.js
+++ routes/signUp.js
@@ -5,6 +5,13 @@ const router = express.Router()
 const ctrls = require('../controllers')
 
 router.get('/signup', ctrls.signUpCtrl.newUser)
+
+router.get('/login', ctrls.signUpCtrl.newSession);
+
+//router.get('/accounts/login',ctrls.signUpCtrl.newSession);
+
 router.post('/signup', ctrls.signUpCtrl.createUser);
+
+
 
 module.exports = router
