293e8db5ac98801dacbbead3ac10b098c8c5f142
Jane Skywalker <janehuizeng@gmail.com>
2019-07-26 11:18:05 -0700

    fixing mongodb connect

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/bonapetite';
+const MONGO_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/bonapetite';
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
